f21a7a75345516c3ef1d71948c53175d30db38df
QingFeng <paradise.qingfeng@gmail.com>
2009-02-19 10:23:27 +0800

    Remove main.Add .gitignore

--- prox.py
+++ prox.py
@@ -6,9 +6,8 @@ e.g
 ./autocurl.py > proxy-list.txt
 '''
 
-if __name__ == '__main__':
-    for i in range(30):
-        url = 'http://www.samair.ru/proxy/proxy-%s.htm' % str(i).zfill(2)
-        html = urllib.urlopen(url).read()
-        for p1,p2,p3,p4,port in re.findall(r'(\d+).(\d+).(\d+).(\d+):(\d+)',html,re.M):
-            print ":".join( [".".join([p1,p2,p3,p4]),port] )
+for i in range(30):
+    url = 'http://www.samair.ru/proxy/proxy-%s.htm' % str(i).zfill(2)
+    html = urllib.urlopen(url).read()
+    for p1,p2,p3,p4,port in re.findall(r'(\d+).(\d+).(\d+).(\d+):(\d+)',html,re.M):
+        print ":".join( [".".join([p1,p2,p3,p4]),port] )
