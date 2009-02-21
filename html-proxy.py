8256582b26b2e58ab72868b99585d00666eafa6c
uc0079 <naruto97d@gmail.com>
2009-02-21 10:37:02 +0800

    修改打开代理列表

--- html-proxy.py
+++ html-proxy.py
@@ -2,10 +2,9 @@ import time
 import os
 import datetime
 
-for r in open("./avlproxy.txt"):
+for r in open("./proxy-list.txt"):
     r = r.strip()
-    curl = 'curl -x %s -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)" -m 10 -r "www.weiphone.com" -L "http://www.weiphone.com/?fromuid=158892"' % r
+    curl = 'curl -x %s -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)" -m 15 -r "www.weiphone.com" -L "http://www.weiphone.com/?fromuid=158892"' % r
     stdin,stdout,stderr=os.popen3(curl)
     if stdout.read().find("Weiphone")!=-1:
         print r
-    time.sleep(2)
