7f2f26ab427b68fb9793436b1289356ad7127cf9
uc0079 <naruto97d@gmail.com>
2009-02-21 10:24:25 +0800

    修改打开路径为当前目录下,推荐prox.py写到./avlproxy.txt

--- html-proxy.py
+++ html-proxy.py
@@ -2,7 +2,7 @@ import time
 import os
 import datetime
 
-for r in open("/Users/uc0079/Desktop/proxy.txt"):
+for r in open("./avlproxy.txt"):
     r = r.strip()
     curl = 'curl -x %s -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)" -m 10 -r "www.weiphone.com" -L "http://www.weiphone.com/?fromuid=158892"' % r
     stdin,stdout,stderr=os.popen3(curl)
